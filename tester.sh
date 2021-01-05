25b40af999fad48d9f87b8af447d7780aa6e8497
gkpotter <gkp@bu.edu>
2021-01-05 15:21:01 -0500

    Fixed formatting.

--- tester.sh
+++ tester.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 log="test.log"
-echo "Advent of Code 2020 (using Python)"
-echo "-------------------------" | tee ${log}
+echo "Advent of Code 2020 (using Python)" | tee ${log}
+echo "-------------------------" | tee -a ${log}
 
 for i in $(seq -f "%02g" 1 25)
 do
-	python3 "day_${i}/main.py" | tee -a  ${log}
+	python3 "days/day_${i}/main.py" | tee -a  ${log}
 	echo "-------------------------" | tee -a  ${log}
 done
-
-# echo "Test saved to: ./${log}"
