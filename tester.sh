1b63e37c354a390f7b2ea5d2db3cf0a4a3d1fad7
gkpotter <gkp@bu.edu>
2021-01-10 16:07:10 -0500

    Updated tester and reran test.

--- tester.sh
+++ tester.sh
@@ -3,26 +3,23 @@
 tmp="test.tmp"
 
 # Test Python
-echo "Advent of Code 2020" | tee ${tmp}
-echo "" | tee -a ${tmp}
-echo "Python" | tee -a ${tmp}
+echo -e "Advent of Code 2020 Test\n\nPython" | tee ${tmp}
 echo "-------------------------" | tee -a ${tmp}
 
 for i in $(seq -f "%02g" 1 25)
 do
-	python3 "days/day_${i}/main.py" | tee -a  ${tmp}
+	python3 days/day_${i}/main.py | tee -a  ${tmp}
 	echo "-------------------------" | tee -a  ${tmp}
 done
 
 # Test OCaml
-echo "" | tee -a ${tmp}
-echo "OCaml" | tee -a ${tmp}
+echo -e "\nOCaml" | tee -a ${tmp}
 echo "-------------------------" | tee -a  ${tmp}
 for i in "01" "05" "06" "09" "10" "13"
 do
-	cd "days/day_${i}/"
+	cd days/day_${i}/
 	corebuild main.native -quiet
-	./main.native | tee -a  "../../${tmp}"
+	./main.native | tee -a  ../../${tmp}
 	corebuild -clean -quiet
 	cd ../..
 	echo "-------------------------" | tee -a  ${tmp}
